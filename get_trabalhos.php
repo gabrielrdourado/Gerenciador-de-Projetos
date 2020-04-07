3f015646ffb1c0a9a16d16ac5b03c6806efe5e89
Gabriel Ribeiro Dourado <dourado.gabrielribeirodourado@gmail.com>
2020-04-07 20:28:09 -0300

    exibir trabalhos semi pronto

--- get_trabalhos.php
+++ get_trabalhos.php
@@ -7,9 +7,7 @@
 	}
 
 	//importando o dbclass
-	require_once('./controllers/db.class.php');
-
-	$id_usuario = $_SESSION['id_usuario'];
+	require_once('controllers/db.class.php');
 
 	//definindo um objeto da classe db
 	$objDB = new db();
@@ -17,8 +15,8 @@
 	//criando o link de conexão com o banco de dados
 	$linkDB = $objDB->conectaMysql();
 
-	$sql = "SELECT t.nome, t.descricao";
-	$sql.= " FROM trabalhos AS t";
+	$sql = "SELECT t.nome, t.descricao, a.nome_arquivo, a.nome_trabalho";
+	$sql.= " FROM trabalhos AS t JOIN arquivos AS a;";
 
 	$resultado_id = mysqli_query($linkDB, $sql);
 
@@ -26,13 +24,18 @@
 		
 		while($registro = mysqli_fetch_array($resultado_id, MYSQLI_ASSOC)){
 			echo '<a href="#" class="list-group-item">';
-				echo '<h4 class="list-item-group-heading">'.$registro['nome']'</h4>';
+				echo '<h4 class="list-item-group-heading">'.$registro['nome'].'</h4>';
 				echo '<p class="list-group-item-text">'.$registro['descricao'].'</p>';
+				echo '<img src="arquivosUpados/';
+				if ($registro['nome'] == $registro['nome_trabalho']) {
+					echo ''.$registro['nome_arquivo'].'';
+				}
+				echo '"/>';
 			echo '</a>';
 		}
 
 	} else {
 		echo('Erro na requisição de trabalhos!!!');
 	}
-
 ?>
+
